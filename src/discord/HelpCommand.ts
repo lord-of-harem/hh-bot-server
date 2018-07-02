1598d3c44aa4483769918ef8063f594933187493
Lord of Harem <lords.of.harem@gmail.com>
2018-07-02 07:14:28 +0200

    remove old code

--- src/discord/HelpCommand.ts
+++ src/discord/HelpCommand.ts
@@ -12,18 +12,6 @@ export default class HelpCommand extends Command
     }
 
     exec(msg): void {
-        function tab(n: number) {
-            return `\t`.repeat(n);
-        }
-
-        /*msg.reply(`Liste des commandes:
-        start/stop boss \`bossId\`${tab(3)}- Gère les combats de boss
-        start/stop pvp${tab(7)}- Gère les combats pvp
-        start/stop harem${tab(6)}- Collecte l'argent du harem
-        start/stop shop${tab(7)}- Actualise automatiquement la page du marché
-        start/stop mission${tab(5)}- Lance automatiquement les mission quotidienne
-        start/stop pachinko${tab(5)}- Récupère dés que possible le cadeau gratuit quotidien
-        stat${tab(12)}- Donne le rapport de ce qu'à fait le système pour la journée en cour`);*/
         msg.channel.send({embed:{
             title: "Help",
             description: "Besoin d'aide ? Pas de panique, toute les commandes disponible sont lister ci-dessous !",
